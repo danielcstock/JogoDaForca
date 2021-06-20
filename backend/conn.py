a9a5405b1c54f6b6ae6e2bac7632f3288297e968
danielcstock <danielcstock12@gmail.com>
2021-06-19 22:12:45 -0300

    -

--- backend/conn.py
+++ backend/conn.py
@@ -11,7 +11,7 @@ class DBConnector():
     def __init__(self):
         # Connect to MariaDB Platform
         try:
-            conn = mariadb.connect(
+            self.conn = mariadb.connect(
                 user="root",
                 password="jdf_abc",
                 host="127.0.0.1",
@@ -23,10 +23,34 @@ class DBConnector():
             sys.exit(1)
 
         # Get Cursor
-        self.cur = conn.cursor()
+        self.cur = self.conn.cursor()
 
-    def select(self, query):
-        self.cur.execute(query)
+    def executeQuery(self, qry):
+        self.cur.execute(qry)
+        response = []
+        for _, value in enumerate(self.cur):
+            response.append(value)
+        return response
+
+    def select(self, table, column, value):
+        qry = "SELECT * FROM " + table + " WHERE " + column + " = " + value
+        self.cur.execute(qry)
+        response = []
+        for _, value in enumerate(self.cur):
+            response.append(value)
+        return response
+
+    def selectValue(self, table, columnResult, column, value):
+        qry = "SELECT " + columnResult + " FROM " + table + " WHERE " + column + " = " + value
+        self.cur.execute(qry)
+        response = []
+        for _, value in enumerate(self.cur):
+            response.append(value)
+        return response
+
+    def count(self, table, column):
+        qry = "SELECT COUNT("+ column +") FROM " + table
+        self.cur.execute(qry)
         response = []
         for _, value in enumerate(self.cur):
             response.append(value)
@@ -34,3 +58,9 @@ class DBConnector():
 
     def insert(self, query):
         self.cur.execute(query)
+        self.conn.commit()
+
+    def update(self, table, column, value, id):
+        qry = "UPDATE " + table + " SET " + column + " = " + str(value) + " WHERE id = " + str(id)
+        self.cur.execute(qry)
+        self.conn.commit()
---
+++ frontend/src/partida.js
@@ -0,0 +1,13 @@
+// Modules to control application life and create native browser window
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+
+function createWindow () {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 400,
+    height: 300,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  }

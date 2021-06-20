322dbc2d0a3ff3c4464f54b4fdf44ce241f75ea3
danielcstock <danielcstock12@gmail.com>
2021-06-20 19:20:13 -0300

    Implementacao comunicacao RPC

--- backend/rpcclient.py
+++ backend/rpcclient.py
@@ -1,4 +1,24 @@
 import xmlrpc.client 
+from roleta import Roleta
 
-proxy = xmlrpc.client .ServerProxy('http://localhost:9000')
-print(proxy.helloWorld())
+if __name__ == '__main__':
+    roleta = Roleta()
+    jogador = input("Nome de jogador: ")
+    proxy = xmlrpc.client .ServerProxy('http://localhost:9000')
+    try:
+        proxy.enter_game(jogador)
+    except:
+        _ = 0
+
+    while(True):
+        pontos = roleta.pontuacaoRodada()
+        letra = input(f"Valendo {pontos} pontos, uma letra: ")
+        try:
+            resposta = proxy.send_letter(jogador, letra, pontos)
+        except:
+            _ = 0
+        if resposta == "Fim do jogo":
+            quit()
+        else:
+            print(resposta)
+        
--- backend/rpcserver.py
+++ backend/rpcserver.py
@@ -2,9 +2,11 @@ from xmlrpc.server import SimpleXMLRPCServer
 import logging
 import os
 
-# Set up logging
-logging.basicConfig(level=logging.DEBUG)
+# Game variables
+players = {}
+round = {}
 
+# Set up logging
 server = SimpleXMLRPCServer(('localhost', 9000), logRequests=False)
 
 # Expose a function
@@ -13,12 +15,58 @@ def list_contents(dir_name):
     return os.listdir(dir_name)
 server.register_function(list_contents)
 
-def helloWorld():
-    print("Hello World")
-server.register_function(helloWorld)
+def enter_game(player):
+    if len(players) < 3:
+        print(f"{player} entrou no jogo.")
+        players[player] = 0
+    else:
+        print("A sala está cheia.")
+    if len(players) == 3:
+        word = start_game(getWord(), getHint())
+server.register_function(enter_game)
+
+def send_letter(player, letter, points):
+    return printWord(player, letter, points)
+server.register_function(send_letter)
+
+def start_game(word, hint):
+    hidden = "_" * len(word)
+    print(f"\nDica: {hint}\n{hidden}")
+    round["hidden"] = hidden
+    round["word"] = word
+
+def getWord():
+    return "abelha"
+
+def getHint():
+    return "animal"
+
+def printWord(player, letter, points):
+    retorno = "Errou"
+    hidden = round["hidden"]
+    word = round["word"]
+    if len(letter) == 1:
+        print(f"{player} - {letter} - {points}")
+        for i, l in enumerate(word):
+            if letter == l and hidden[i] == "_":
+                hidden = hidden[:i] + letter + hidden[i+1:]
+                players[player] += int(points)
+                round["hidden"] = hidden
+                retorno = "Acertou"
+    print(hidden)
+    if checkWord(hidden):
+        print(f"Placar final:\n{players}")
+        server.server_close()
+        retorno = "Fim do jogo"
+    return retorno
+
+def checkWord(hidden):
+    if "_" in hidden:
+        return False
+    return True
 
 try:
-    print("Use Control-C to exit")
+    print("Aguardando jogadores")
     server.serve_forever()
 except KeyboardInterrupt:
     print("Exiting")
